961350c2d8ad99062be410fa40c24ec199636731
Celil Tat <celilbtc@gmail.com>
2023-12-10 02:51:13 +0100

    Added GenericRepository and wrote some method

--- EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -10,6 +10,18 @@ namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidatio
 {
 	public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
 	{
-
+		public AppUserRegisterValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Name required");
+			RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname required");
+			RuleFor(x => x.UserName).NotEmpty().WithMessage("User name required");
+			RuleFor(x => x.Email).NotEmpty().WithMessage("Email required");
+			RuleFor(x => x.Password).NotEmpty().WithMessage("Password required");
+			RuleFor(x => x.Surname).NotEmpty().WithMessage("Comfirm Password required");
+			RuleFor(x => x.Name).MaximumLength(30).WithMessage("It must be Maximum 30 character");
+			RuleFor(x => x.Name).MinimumLength(2).WithMessage("It must be Minimum 2 character");
+			RuleFor(x => x.ComfirmPassword).Equal(y => y.Password).WithMessage("It must be same password value");
+			RuleFor(x => x.Email).EmailAddress().WithMessage("Please write true mail format");
+		}
 	}
 }
