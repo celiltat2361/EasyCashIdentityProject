333e48cc7bdf4b344e37e33de077223a0804c638
Celil Tat <celilbtc@gmail.com>
2023-12-10 01:50:51 +0100

    Started dto layer and added fluent Validation package

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+	public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+	{
+
+	}
+}
---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+	public class AppUserRegisterDto
+	{
+
+		//[Required(ErrorMessage = "Name required")]
+		//[Display(Name = "Name")]
+		//[MaxLength(30, ErrorMessage = "Max 30 character")]
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string UserName { get; set; }
+		public string Surname { get; set; }
+		public string ComfirmPassword { get; set; }
+	}
+}
+
+//name,surname, username, mail, password, comfirmpasword
