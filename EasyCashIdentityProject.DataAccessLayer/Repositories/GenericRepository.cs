62e7fcd80972e0150be18305f5340096ee651db5
Celil Tat <celilbtc@gmail.com>
2023-12-10 02:51:46 +0100

    added methods

--- EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Repositories
 {
-	internal class GenericRepository
+	public class GenericRepository<T> : IGenericDal<T> where T : class
 	{
+		public void Delete(T t)
+		{
+			using var context = new Context();
+			context.Set<T>().Remove(t);
+			context.SaveChanges();
+		}
+
+		public T GetByID(int id)
+		{
+			using var context = new Context();
+			return context.Set<T>().Find(id);
+		}
+
+		public List<T> GetList()
+		{
+			using var context = new Context();
+			return context.Set<T>().ToList();
+		}
+
+		public void Insert(T t)
+		{
+			using var context = new Context();
+			context.Set<T>().Add(t);
+			context.SaveChanges();
+		}
+
+		public void Update(T t)
+		{
+			using var context = new Context();
+			context.Set<T>().Update(t);
+			context.SaveChanges();
+		}
 	}
 }
